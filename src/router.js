22d608e0bd6873d8d377361f3ee13e40c5431853
niel <niel@tentwenty.me>
2018-08-30 10:58:27 +0400

    Add transitions

--- src/router.js
+++ src/router.js
@@ -1,16 +1,16 @@
-import React, { Component } from "react";
-import { Router, Route, Switch, Redirect } from "react-router-dom";
-import { TransitionGroup, CSSTransition } from "react-transition-group";
-import { TweenLite, TweenMax, Power2 } from "gsap";
-import PageTitle from "./pages/PageTitle";
-import PageDetailsTypeA from "./pages/PageDetails/TypeA";
-import PageDetailsTypeB from "./pages/PageDetails/TypeB";
-import PageDetailsTypeC from "./pages/PageDetails/TypeC";
-import PageDetailsTypeD from "./pages/PageDetails/TypeD";
-import Renderer from "./components/Renderer";
-import history from "./history";
-import config from "./config";
-import { animateHeader, animateCount } from "./scripts/utils";
+import React, { Component } from 'react';
+import { Router, Route, Switch, Redirect } from 'react-router-dom';
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
+import { TweenLite, TweenMax, Power4, TimelineMax } from 'gsap';
+import PageTitle from './pages/PageTitle';
+import PageDetailsTypeA from './pages/PageDetails/TypeA';
+import PageDetailsTypeB from './pages/PageDetails/TypeB';
+import PageDetailsTypeC from './pages/PageDetails/TypeC';
+import PageDetailsTypeD from './pages/PageDetails/TypeD';
+import Renderer from './components/Renderer';
+import history from './history';
+import config from './config';
+import { animateHeader, animateCount } from './scripts/utils';
 
 class AppRouter extends Component {
   constructor(props) {
@@ -19,7 +19,7 @@ class AppRouter extends Component {
       routeList: [],
       forward: true,
       mounted: false,
-      node: null
+      node: null,
     };
   }
   componentDidMount() {
@@ -28,7 +28,7 @@ class AppRouter extends Component {
     //   this.initAnimate();
     // }, 500);
 
-    document.addEventListener("keydown", event => {
+    document.addEventListener('keydown', event => {
       // const keyName = event.key;
       const keyCode = event.which;
       let page = history.location.pathname.substr(1);
@@ -69,19 +69,19 @@ class AppRouter extends Component {
   getTemplate = (template, params) => {
     let temp = null;
     switch (template) {
-      case "page-title":
+      case 'page-title':
         temp = <PageTitle params={params} />;
         break;
-      case "page-details-a":
+      case 'page-details-a':
         temp = <PageDetailsTypeA params={params} />;
         break;
-      case "page-details-b":
+      case 'page-details-b':
         temp = <PageDetailsTypeB params={params} />;
         break;
-      case "page-details-c":
+      case 'page-details-c':
         temp = <PageDetailsTypeC params={params} />;
         break;
-      case "page-details-d":
+      case 'page-details-d':
         temp = <PageDetailsTypeD params={params} />;
         break;
       default:
@@ -91,105 +91,103 @@ class AppRouter extends Component {
   };
 
   handleComplete = target => {
-    TweenLite.set(target, { clearProps: "all" });
+    TweenLite.set(target, { clearProps: 'all' });
     this.initAnimate();
   };
 
   initAnimate = () => {
+    console.log('init');
+    return;
     animateHeader();
     animateCount();
     window.AOS.init();
-    console.log("init");
   };
 
   onExiting = node => {
     if (!node) return;
     const { forward } = this.state;
     // Kill animation
-    TweenMax.killTweensOf(node);
-    TweenLite.killTweensOf(node);
+    // TweenMax.killTweensOf(node);
+    // TweenLite.killTweensOf(node);
     // TweenLite.to(node, 0.6, { xPercent: this.state.forward ? -100 : 100 });
-    // TweenLite.set(node, { scale: 0.3 });
-    // TweenLite.to(node, 0.6, { xPercent: forward ? -80 : 50 });
-    TweenLite.fromTo(
-      node,
-      0.6,
-      {
-        opacity: 1,
-        scale: 1
+    console.log('exiting', node, window);
+    const tl = new TimelineMax({
+      onComplete: () => {
+        console.log('exit');
       },
-      {
-        opacity: 0,
-        scale: 0.8,
-        clearProps: "opacity, scale",
-        onComplete: function() {
-          // removeClass(el, ['is-onTop', 'is-current']);
-          console.log("exiting");
-        }
-      }
-    );
+    });
+
+    tl.set(node, {
+      xPercent: 0,
+      scale: 1,
+      transformOrigin: '50% 50%',
+    });
+
+    tl.to(node, 0.3, {
+      scale: 0.5,
+    });
+
+    tl.to(node, 1.5, {
+      xPercent: this.state.forward ? -100 : 100,
+    });
   };
 
   onEntering = (node, isAppearing) => {
     if (!node) return;
     const { forward } = this.state;
-    // Kill animation
-    TweenMax.killTweensOf(node);
-    TweenLite.killTweensOf(node);
-    TweenLite.set(node, {
+    // New
+    const tl = new TimelineMax({
+      onComplete: () => {
+        console.log('done enter');
+      },
+    });
+
+    tl.set(node, {
       scale: 0.5,
+      xPercent: forward ? 100 : -100,
+      opacity: 0.5,
+    });
+
+    tl.to(node, 0.3, {
       xPercent: forward ? 50 : -50,
-      opacity: 0.5
     });
-    TweenLite.to(node, 0.5, {
+
+    tl.to(node, 0.8, {
       xPercent: 0,
-      opacity: 1
-      // transformPerspective: 200
-      // onComplete: this.handleComplete,
-      // onCompleteParams: [node]
+      opacity: 1,
     });
-    TweenLite.to(node, 0.9, {
+
+    tl.to(node, 1.5, {
       scale: 1,
       onComplete: this.handleComplete,
-      onCompleteParams: [node]
-    }).delay(0.5);
-    // TweenLite.set(node, {
-    //   visibility: "visible",
-    //   xPercent: this.state.forward ? 100 : -100,
-    //   position: "fixed",
-    //   left: 0,
-    //   top: 0,
-    //   right: 0
-    // });
-    // TweenLite.to(node, 0.6, {
-    //   xPercent: 0,
-    //   onComplete: this.handleComplete,
-    //   onCompleteParams: [node]
-    // });
+      ease: Power4.easeOut,
+      onCompleteParams: [node],
+    });
   };
 
   render() {
     const { forward } = this.state;
     return (
       <Router history={history}>
+        {/* <Route exact path="/" render={() => <Redirect to="/1" />} /> */}
         <Route
-          render={location => {
+          render={({ location }) => {
             console.log(location);
             return (
               <TransitionGroup>
                 <CSSTransition
-                  timeout={600}
+                  timeout={2000}
                   classNames="transition"
                   // unmountOnExit={true}
                   // mountOnEnter={true}
                   appear={true}
-                  key={location.location.key}
+                  key={location.pathname}
                   onExit={node => this.onExiting(node)}
                   onEnter={(node, isAppearing) => {
                     this.onEntering(node, isAppearing);
                   }}
                 >
-                  <Switch location={location.location}>
+                  <Switch location={location}>
                     {config.map((item, index) => {
                       item.forward = forward;
                       return (
@@ -202,6 +200,7 @@ class AppRouter extends Component {
                         />
                       );
                     })}
+                    <Route exact path="/" render={() => <Redirect to="/1" />} />
                   </Switch>
                 </CSSTransition>
               </TransitionGroup>
