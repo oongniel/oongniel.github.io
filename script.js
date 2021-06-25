df8801e31b4843bf61380107d869578ffd386e49
Niel Ong <niel@tentwenty.me>
2021-06-25 21:56:31 +0400

    add search icon

--- script.js
+++ script.js
@@ -1,21 +1,28 @@
 $(() => {
-  var typed4 = new Typed("#type-animation", {
-    strings: [
-      "Hello... you there?? :)",
-      "Pls save the date...",
-      "Drinks are on US!",
-      "Hold on....",
-      // "...07.07.2021",
-      "and oh!",
-      "...",
-      "????",
-      "WE'RE GETTING MARRIED! &lt;3 &lt;3",
-    ],
-    typeSpeed: 60,
-    backSpeed: 30,
-    loop: false,
-    onComplete: () => {
-      console.log("Done");
-    },
-  });
+  const initTyped = () => {
+    new Typed("#type-animation", {
+      strings: [
+        "Hello... you there?? :)",
+        "Pls save the date...",
+        "Drinks are on US!",
+        "Hold on....",
+        // "...07.07.2021",
+        "and oh!",
+        "...",
+        "????",
+        "WE'RE GETTING MARRIED!&lt;3",
+      ],
+      typeSpeed: 60,
+      backSpeed: 30,
+      loop: false,
+      onComplete: () => {
+        console.log("Done");
+      },
+    });
+  };
+  const init = () => {
+    initTyped();
+  };
+  init();
+  // var typed4 =;
 });
